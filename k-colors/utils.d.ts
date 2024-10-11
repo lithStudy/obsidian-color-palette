aeb7f7235243c53bdf752e6a65ac2ca048f330e0
lith <lith@guahao.com>
2024-10-11 18:49:18 +0800

    色板工具改造

--- k-colors/utils.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function get_image_data(canvas: HTMLImageElement | OffscreenCanvas | HTMLCanvasElement | ImageData): ImageData;
--- src/thief/thiefApp.js
+++ src/thief/thiefApp.js
@@ -1,11 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import ColorThief from 'colorthief';
+import ColorsRow from 'src/components/colors-row'
+import {hexToNumber} from "../utils/utils";
+import Color from "color";
+
+import { InboxOutlined } from '@ant-design/icons';
+import { message, Upload } from 'antd';
+
+const { Dragger } = Upload;
 
 const ThiefApp = () => {
-	const [colorArr, setColorArr] = useState([]);
+
+	const [mainColor, setMainColor] = useState([]);
+	const [colorPalette,setColorPalette] = useState([]);
 	const [selectedImage, setSelectedImage] = useState(null);
 
 	const colorThief = new ColorThief();
+
+	useEffect(() => {
+		if (selectedImage) {
+			extractColors(selectedImage);
+		}
+	}, [selectedImage]);
+
+	const props = {
+		name: 'file',
+		multiple: true,
+		// action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
+		onChange(info) {
+			const { status } = info.file;
+			debugger
+			if (status !== 'uploading') {
+				console.log(info.file, info.fileList);
+			}
+			if (status === 'done') {
+				const reader = new FileReader();
+				reader.onload = (e) => {
+					const img = new Image();
+					img.src = e.target.result;
+					img.onload = () => {
+						setSelectedImage(img);
+						// extractColors(img);
+					};
+				};
+				reader.readAsDataURL(info.file.originFileObj);
+			} else if (status === 'error') {
+				console.log('Load failed');
+			}
+
+
+		},
+		onDrop(e) {
+			console.log('Dropped files', e.dataTransfer.files);
+		},
+	};
+
 
 	const handleImageChange = (event) => {
 		const file = event.target.files[0];
@@ -16,7 +65,7 @@ const ThiefApp = () => {
 				img.src = e.target.result;
 				img.onload = () => {
 					setSelectedImage(img);
-					extractColors(img);
+					// extractColors(img);
 				};
 			};
 			reader.readAsDataURL(file);
@@ -24,29 +73,62 @@ const ThiefApp = () => {
 	};
 
 	const extractColors = (img) => {
-		const colors = colorThief.getColor(img);
-		setColorArr(colors);
+		const mainColorTemp = colorThief.getColor(img);
+		const mainColorRgb = hexToNumber(Color(`rgb(${mainColorTemp[0]}, ${mainColorTemp[1]}, ${mainColorTemp[2]})`).hex())
+		setMainColor(mainColorRgb);
+
+		let paletteTemp = colorThief.getPalette(img);
+		const colorsList = []
+		paletteTemp.forEach(colorTemp => {
+			colorsList.push(Color(`rgb(${colorTemp[0]}, ${colorTemp[1]}, ${colorTemp[2]})`).hex())
+		})
+		setColorPalette(colorsList);
+
+
 	};
 
-	useEffect(() => {
-		if (selectedImage) {
-			extractColors(selectedImage);
-		}
-	}, [selectedImage]);
+
 
 	return (
 		<div>
-			<h1>Color Extractor</h1>
-			<input type="file" accept="image/*" onChange={handleImageChange} />
-			{selectedImage && (
-				<img src={selectedImage.src} alt="Selected Image" style={{ maxWidth: '100%' }} />
-			)}
-			{colorArr.length > 0 && (
-				<div>
-					<h2>Extracted Colors:</h2>
-					<p>{JSON.stringify(colorArr)}</p>
-				</div>
-			)}
+			<div>
+				<h1>Color Extractor</h1>
+				<Dragger {...props}>
+					<p className="ant-upload-drag-icon">
+						<InboxOutlined/>
+					</p>
+					<p className="ant-upload-text">Click or drag file to this area to upload</p>
+					<p className="ant-upload-hint">
+						Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+						banned files.
+					</p>
+				</Dragger>
+
+				{/*<input type="file" accept="image/*" onChange={handleImageChange}/>*/}
+
+				{selectedImage && (
+					<div style={{marginBottom: '20px'}}>
+						<div>
+							<img src={selectedImage.src} alt="Selected Image" style={{maxWidth: '500px'}}/>
+						</div>
+						<div>
+
+						</div>
+
+					</div>
+				)}
+				{mainColor.length > 0 && (
+					<ColorsRow
+						mainColor={mainColor}
+						darkColors={[]}
+						lightColors={colorPalette}
+					/>
+				)}
+
+			</div>
+
+
+
 		</div>
 	);
 };
